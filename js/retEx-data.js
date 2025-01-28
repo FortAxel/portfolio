fad67a52bbc8578527af8a6c8bd13cbe4eca173c
FortAxel <fortunatoaxel1411@gmail.com>
2025-01-28 23:04:51 +0100

    performance optimisation

--- js/retEx-data.js
+++ js/retEx-data.js
@@ -2,7 +2,7 @@ const retexData = {
     'towa': {
         linkTitle: 'IA pour un jeu de plateau (Java)',
         title: 'Jeu de plateau Java avec IA',
-        image: './img/illustration_towa.png',
+        image: './img/illustration_towa.webp',
         requirements: [
             'Implémenter des règles de plus en plus complexes sur un jeu de plateau 16*16 donné.',
             'Prévoir les actions possibles en s\'adaptant à la situation de jeu',
@@ -16,12 +16,12 @@ const retexData = {
             'Versionner un répertoire GIT',
             'Hiérarchiser des actions pour gagner'
         ],
-        toolLogos: ['./img/logo_java.png']
+        toolLogos: ['./img/logo_java.webp']
     },
     'robocup': {
         linkTitle: 'Application web de gestion de compétitions (Symfony)',
         title: 'Application web de gestion de compétitions',
-        image: './img/illustration_robocup.png',
+        image: './img/illustration_robocup.webp',
         requirements: [
             'Développer une application web avec Symfony',
             'Gérer une base de données',
@@ -33,12 +33,12 @@ const retexData = {
             'Gestion de base de données',
             'Conception d\'interface utilisateur'
         ],
-        toolLogos: ['./img/logo_symfony.png']
+        toolLogos: ['./img/logo_symfony.webp']
     },
     'reseau': {
         linkTitle: 'Configuration réseau avec NextCloud (Bash)',
         title: 'Configuration réseau avec serveur NextCloud',
-        image: './img/illustration_reseau.png',
+        image: './img/illustration_reseau.webp',
         requirements: [
             'Créer un réseau sur une machine, virtuelle avec deux ordinateurs, deux serveurs et un routeur',
             'Permettre le travail groupé dans une équipe',
@@ -50,12 +50,12 @@ const retexData = {
             'Administrer un réseau',
             'Rédiger un tutoriel en Anglais'
         ],
-        toolLogos: ['./img/logo_bash.png']
+        toolLogos: ['./img/logo_bash.webp']
     },
     'pixelwar': {
         linkTitle: 'Client Pixel Waj (js)',
         title: 'Client Pixel War',
-        image: './img/illustration_pixelWar.png',
+        image: './img/illustration_pixelWar.webp',
         requirements: [
             'Créer interface utilisable pour 1 joueur à une pixel war',
             'Afficher et actualiser la grille et les joueurs en utilisant une API propriétaire'
@@ -67,30 +67,30 @@ const retexData = {
             'Programmation évenementielle',
             'Requêtes serveur en JavaScript'
         ],
-        toolLogos: ['./img/logos_web.png']
+        toolLogos: ['./img/logo_web.webp']
     },
     'unideck': {
         linkTitle: 'Jeu de cartes 1 vs 1 (C#)',
         title: 'Jeu de cartes 1 vs 1',
-        image: './img/illustration_uniDeck.png',
+        image: './img/illustration_uniDeck.webp',
         requirements: [
             'Créer l\'intéraction entre le jeu et les joueurs',
             'Ajouter des fonctionnalités pour créer la complexité',
             'Adapter l\'affichage en fonction des possibilités',
             'Créer le diagramme UML du projet'
         ],
-        tools: ['C#', 'UML', 'bbbbbbbbb'],
+        tools: ['C#', 'UML'],
         skills: [
             'Programmation évenementielle logicielle',
             'Modéliser un programme en détail',
             'Simplifier la compréhension d\'un code'
         ],
-        toolLogos: ['./img/logo_cs.png']
+        toolLogos: ['./img/logo_cs.webp']
     },
     'java': {
         linkTitle: 'Atelier d\'initiation au java (java)',
         title: 'Réaliser un atelier initiation au Java',
-        image: './img/illustration_java.png',
+        image: './img/illustration_java.webp',
         requirements: [
             'Créer un jeu en Java et en créer un exercice pour les débutants visant à leur faire finir le programme',
             'Faire un tutoriel intuitif et chaleureux pour encourager ces débutants à en apprendre plus'
@@ -101,7 +101,7 @@ const retexData = {
             'Produire un code compréhensible',
             'Expliquer un programme et des concepts avec des mots simples'
         ],
-        toolLogos: ['./img/logo_java.png']
+        toolLogos: ['./img/logo_java.webp']
     }
 };
 
